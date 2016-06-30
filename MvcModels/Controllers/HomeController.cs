07777b341b86d2c56ad78025ce1ae0194078bc0d
JeanSchwalenberg <jeanschwalenberg@gmail.com>
2016-06-30 17:35:30 -0600

    Initial commit (up to but not incl "Understanding Model Binding")

---
+++ MvcModels/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcModels.Models;
+
+namespace MvcModels.Controllers {
+    public class HomeController : Controller {
+        private Person[] personData = {
+            new Person {PersonId = 1, FirstName = "Adam", LastName = "Freeman",
+                Role = Role.Admin},
+            new Person {PersonId = 2, FirstName = "Jacqui", LastName = "Griffyth",
+                Role = Role.User},
+            new Person {PersonId = 3, FirstName = "John", LastName = "Smith",
+                Role = Role.User},
+            new Person {PersonId = 4, FirstName = "Anne", LastName = "Jones",
+                Role = Role.Guest}
+        };
+        public ActionResult Index(int id) {
+            Person dataItem = personData.Where(p => p.PersonId == id).First();
+            return View(dataItem);
+        }
+    }
+}
