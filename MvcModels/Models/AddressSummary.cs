651c3444631e2e86e7c06ae943cf919ff303e665
JeanSchwalenberg <jeanschwalenberg@gmail.com>
2016-06-30 19:06:43 -0600

    up to "Specifying Custom Prefixes" including 24-14

---
+++ MvcModels/Models/AddressSummary.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcModels.Models {
+    public class AddressSummary {
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
