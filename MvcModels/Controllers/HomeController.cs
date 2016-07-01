85a0de84afd3a8d6fd01f8278db45e7fe87b3a6b
JeanSchwalenberg <jeanschwalenberg@gmail.com>
2016-06-30 23:15:05 -0600

    up to Removing Bind Attribute from AddressSummary.cs
    but not including Manually Invoking Model Binding

--- MvcModels/Controllers/HomeController.cs
+++ MvcModels/Controllers/HomeController.cs
@@ -31,5 +31,19 @@ namespace MvcModels.Controllers {
         public ActionResult CreatePerson(Person model) {
             return View("Index", model);
         }
+
+        public ActionResult DisplaySummary([Bind(Prefix="HomeAddress", Exclude="Country")]AddressSummary summary) {
+            return View(summary);
+        }
+
+        public ActionResult Names(string[] names) {
+            names = names ?? new string[0];
+            return View(names);
+        }
+
+        public ActionResult Address(IList<AddressSummary> addresses) {
+            addresses = addresses ?? new List<AddressSummary>();
+            return View(addresses);
+        }
     }
 }
--- MvcModels/Models/AddressSummary.cs
+++ MvcModels/Models/AddressSummary.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web.Mvc;
 
 namespace MvcModels.Models {
+    //[Bind(Include="City")]
     public class AddressSummary {
         public string City { get; set; }
         public string Country { get; set; }
